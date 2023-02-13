21e4cf7eb669f5bac9031088a78aa9e18b434a3b
IlyaAfanasev <gecktor.86@mail.ru>
2023-02-13 21:26:01 +0400

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1 () {
+        var dog=8;
+        System.out.println(dog);
+
+    }
+
+    public static void task2 () {
+        var cat=3.6;
+        System.out.println(cat);
+
+    }
+    public static void task3 () {
+        var paper=763789;
+        System.out.println(paper);
+
+    }
+}
