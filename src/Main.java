4964812bc9240e2fe89417a7cf85adcfc9420ce9
IlyaAfanasev <gecktor.86@mail.ru>
2023-02-13 21:28:59 +0400

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,22 @@ public class Main {
     public static void task1 () {
         var dog=8;
         System.out.println(dog);
+        dog=dog+4;
+        System.out.println(dog);
 
     }
 
     public static void task2 () {
         var cat=3.6;
         System.out.println(cat);
+        cat=cat+4;
+        System.out.println(cat);
 
     }
     public static void task3 () {
         var paper=763789;
+        System.out.println(paper);
+        paper=paper+4;
         System.out.println(paper);
 
     }
